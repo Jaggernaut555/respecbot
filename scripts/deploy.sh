5256185fb0ada52286309978f86290af57afa479
Jaggernaut555 <jay_jag6@hotmail.com>
2017-11-25 19:03:50 -0800

    Updated deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,14 +6,14 @@ PULLREQ=$3
 case "$BRANCH" in
 "master" )
     if [[ $PULLREQ == 'false' ]]; then
-        curl -H "Content-Type: application/json" -X POST -d '{"token":"'"$TOKEN"'", "branch":"'"$BRANCH"'"}' http://jaggernaut.ca:9000/hooks/deploy-respecbot-webhook;
+        curl -H "Content-Type: application/json" -X POST -d '{"token":"'"$TOKEN"'", "branch":"'"$BRANCH"'"}' http://jaggernaut.ca:9000/hooks/deploy-respecbot-webhook ;
     else
         echo "Not Deploying a pull request"
     fi
     ;;
 "staging" )
     if [[ $PULLREQ == 'false' ]]; then
-        curl -H "Content-Type: application/json" -X POST -d '{"token":"'"$TOKEN"'", "branch":"'"$BRANCH"'"}' http://jaggernaut.ca:9000/hooks/deploy-respecbot-webhook;
+        curl -H "Content-Type: application/json" -X POST -d '{"token":"'"$TOKEN"'", "branch":"'"$BRANCH"'"}' http://jaggernaut.ca:9000/hooks/deploy-respecbot-webhook ;
     else
         echo "Not Deploying a pull request"
     fi
