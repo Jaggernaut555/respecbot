3a25f2b8b55e54d2d5b36ee1824b516ed0331371
Jaggernaut555 <jay_jag6@hotmail.com>
2017-11-26 16:55:45 -0800

    fixed typo in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,14 +7,14 @@ echo "START DEPLOY"
 case "$BRANCH" in
 "master" )
     if [[ $PULLREQ == 'false' ]]; then
-        curl -H "Content-Type: application/json" -X POST -d '{"token":"'"$TOKEN"'", "branch":"'"$BRANCH"'"}' http://jaggernaut.ca:9000/hooks/deploy-respecbot-webhook ;
+        curl -H "Content-Type: application/json" -X POST -d '{"token":"'"$TOKEN"'", "branch":"'"$BRANCH"'"}' http://jaggernaut.ca:9000/hooks/deploy-respecbot-webhook
     else
         echo "Not Deploying a pull request"
     fi
     ;;
 "staging" )
     if [[ $PULLREQ == 'false' ]]; then
-        curl -H "Content-Type: application/json" -X POST -d '{"token":"'"$TOKEN"'", "branch":"'"$BRANCH"'"}' http://jaggernaut.ca:9000/hooks/deploy-respecbot-webhook ;
+        curl -H "Content-Type: application/json" -X POST -d '{"token":"'"$TOKEN"'", "branch":"'"$BRANCH"'"}' http://jaggernaut.ca:9000/hooks/deploy-respecbot-webhook
     else
         echo "Not Deploying a pull request"
     fi
